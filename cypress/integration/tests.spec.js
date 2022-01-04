890def1b4b9ef7528446f2b84ea05fd2293bdd6f
Max Evstafev <emaxiss@gmail.com>
2022-01-03 19:40:19 -0500

    project conf and inital test

---
+++ cypress/integration/tests.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe("test", ()=> {
+    it('visit the page', ()=> {
+        cy.visit('https://www.way2automation.com/angularjs-protractor/webtables');
+    })
+});
